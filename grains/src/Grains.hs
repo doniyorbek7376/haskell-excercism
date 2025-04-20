8a98010ba614d1173995b442c775bb156787dbc2
Doniyorbek Urinboyev <doniyorbek7376@gmail.com>
2025-04-20 09:07:58 +0500

    solve: grains

--- grains/src/Grains.hs
+++ grains/src/Grains.hs
@@ -1,18 +1,10 @@
 module Grains (square, total) where
 
-import Data.Maybe (fromJust)
-
 square :: Integer -> Maybe Integer
 square x
   | x <= 0 = Nothing
   | x > 64 = Nothing
-  | x == 1 = Just 1
   | otherwise = Just $ 2 ^ (x - 1)
 
 total :: Integer
-total = sumMaybe $ map square [1 .. 64]
-
-sumMaybe :: [Maybe Integer] -> Integer
-sumMaybe [] = 0
-sumMaybe (Nothing : xs) = sumMaybe xs
-sumMaybe (x : xs) = fromJust x + sumMaybe xs
+total = 2 ^ (64 :: Integer) - 1
